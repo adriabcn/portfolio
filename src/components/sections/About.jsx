2597dda539bb59168918ef335f1d1a9cc73fe94b
Adrià Aubanell Cabezas <adriaaubanellcabezas@Adrias-MacBook-Air.local>
2025-05-24 17:14:33 +0200

    ℹ️ Update About section content for new visualization

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -8,11 +8,11 @@ export const About = () => {
     const ciCdSkills = ["Jenkins"];
     const iaCSkills = ["Kind", "Terraform"];
     const cloudSkills = ["AWS"];
-    const monitoringSkills = ["Prometheus", "Grafana", "Elasticsearch", "Kibana"];
+    //const monitoringSkills = ["Prometheus", "Grafana", "Elasticsearch", "Kibana"];
     const versionControlSkills = ["Git", "GitHub"];
 
-    const frontendSkills = ["Vite", "React", "Tailwind CSS", "TypeScript", "Axios"];
-    const backendSkills = ["Node", "Symfony", "MySQL", "MongoDB"];
+    const frontendSkills = ["Vite", "React", "Tailwind CSS", "GSAP", "TypeScript", "Axios"];
+    const backendSkills = ["Node", "MySQL", "MongoDB"];
     const semSkills = ["Google Ads", "Google Trends", "Google Analytics", "Meta Ads"];
     const aiSkills = ["ChatGPT", "Midjourney", "Leonardo AI", "Qwen"];
 
@@ -29,9 +29,9 @@ export const About = () => {
                     </p>
                     <div className="rounded-xl p-8 border-black/10 dark:border-white/10 border hover:-translate-y-1 transition-all">
                         
-                        <h3 className="text-xl font-bold mb-4">Skills and Programs as a DevOps</h3>
+                        <h3 className="text-xl font-bold mb-4">Skills and Programs – Junior DevOps</h3>
 
-                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-6">
+                        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-6">
                             <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
                                 <h4 className="text-xl font-bold mb-4">Containers</h4>
                                 <div className="flex flex-wrap gap-2">
@@ -72,7 +72,7 @@ export const About = () => {
                                     ))}
                                 </div>
                             </div>
-                            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                            {/*<div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
                                 <h4 className="text-xl font-bold mb-4">Monitoring</h4>
                                 <div className="flex flex-wrap gap-2">
                                     {monitoringSkills.map((tech, key) => (
@@ -81,7 +81,7 @@ export const About = () => {
                                         </span>
                                     ))}
                                 </div>
-                            </div>
+                            </div>*/}
                             <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
                                 <h4 className="text-xl font-bold mb-4">Repos</h4>
                                 <div className="flex flex-wrap gap-2">
@@ -95,50 +95,60 @@ export const About = () => {
                         </div>
                     </div>
 
-                    <div className="rounded-xl p-8 border-black/10 dark:border-white/10 border hover:-translate-y-1 transition-all  mt-8">
-                        
-                        <h3 className="text-xl font-bold mb-4">Other Skills and Programs</h3>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
 
-                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-6">
-                            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                                <h4 className="text-xl font-bold mb-4">Frontend</h4>
-                                <div className="flex flex-wrap gap-2">
-                                    {frontendSkills.map((tech, key) => (
-                                        <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition">
-                                            {tech}
-                                        </span>
-                                    ))}
-                                </div>
-                            </div>
-                            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                                <h4 className="text-xl font-bold mb-4">Backend</h4>
-                                <div className="flex flex-wrap gap-2">
-                                    {backendSkills.map((tech, key) => (
-                                        <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 2246, 0.2)] transition">
-                                            {tech}
-                                        </span>
-                                    ))}
-                                </div>
-                            </div>
+                        <div className="p-6 rounded-xl border-black/10 dark:border-white/10 border hover:-translate-y-1 transition-all">
+                            
+                            <h3 className="text-xl font-bold mb-4">Skills and Programs – Junior Full Stack Developer</h3>
 
-                            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                                <h4 className="text-xl font-bold mb-4">SEM</h4>
-                                <div className="flex flex-wrap gap-2">
-                                    {semSkills.map((tech, key) => (
-                                        <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition">
-                                            {tech}
-                                        </span>
-                                    ))}
+                            <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+                                <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                                    <h4 className="text-xl font-bold mb-4">Frontend</h4>
+                                    <div className="flex flex-wrap gap-2">
+                                        {frontendSkills.map((tech, key) => (
+                                            <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition">
+                                                {tech}
+                                            </span>
+                                        ))}
+                                    </div>
+                                </div>
+                                <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                                    <h4 className="text-xl font-bold mb-4">Backend</h4>
+                                    <div className="flex flex-wrap gap-2">
+                                        {backendSkills.map((tech, key) => (
+                                            <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 2246, 0.2)] transition">
+                                                {tech}
+                                            </span>
+                                        ))}
+                                    </div>
                                 </div>
                             </div>
-                            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                                <h4 className="text-xl font-bold mb-4">AI</h4>
-                                <div className="flex flex-wrap gap-2">
-                                    {aiSkills.map((tech, key) => (
-                                        <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 2246, 0.2)] transition">
-                                            {tech}
-                                        </span>
-                                    ))}
+                        </div>
+
+                        <div className="p-6 rounded-xl border-black/10 dark:border-white/10 border hover:-translate-y-1 transition-all">
+                            
+                            <h3 className="text-xl font-bold mb-4">Other Skills and Programs</h3>
+
+                            <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+                                <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                                    <h4 className="text-xl font-bold mb-4">SEM</h4>
+                                    <div className="flex flex-wrap gap-2">
+                                        {semSkills.map((tech, key) => (
+                                            <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition">
+                                                {tech}
+                                            </span>
+                                        ))}
+                                    </div>
+                                </div>
+                                <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                                    <h4 className="text-xl font-bold mb-4">AI</h4>
+                                    <div className="flex flex-wrap gap-2">
+                                        {aiSkills.map((tech, key) => (
+                                            <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 2246, 0.2)] transition">
+                                                {tech}
+                                            </span>
+                                        ))}
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -150,19 +160,19 @@ export const About = () => {
                             <div className="space-y-4 text-gray-500 dark:text-gray-300">
                                 <div>
                                     Bachelor's degree in Translation and Interpreting
-                                    <p className="text-gray-400 mb-2">UPF (Barcelona, 2021-2025)</p>
+                                    <p className="text-gray-400 mb-2">UPF (Barcelona [ES], 2021-2025)</p>
                                 </div>
                                 <div>
                                     Erasmus - Bachelor's degree in Translation and Interpreting
-                                    <p className="text-gray-400 mb-2">Université de Lorraine (Nancy, 2023)</p>
+                                    <p className="text-gray-400 mb-2">Université de Lorraine (Nancy [FR], 2023)</p>
                                 </div>
                                 <div>
                                     Advanced vocational trining, Web Application Development
-                                    <p className="text-gray-400 mb-2">INS La Pineda (Badalona, 2018-2020)</p>
+                                    <p className="text-gray-400 mb-2">INS La Pineda (Badalona [ES], 2018-2020)</p>
                                 </div>
                                 <div>
                                     Intermediate vocational trining, Microcomputer Systems and Networks
-                                    <p className="text-gray-400 mb-2">Escoles Freta (Calella, 2016-2018)</p>
+                                    <p className="text-gray-400 mb-2">Escoles Freta (Calella [ES], 2016-2018)</p>
                                 </div>
                             </div>
                         </div>
@@ -197,6 +207,7 @@ export const About = () => {
                             <h3 className="text-xl font-bold mb-4">Hobbies</h3>
                             <ul className="list-disc list-inside text-gray-500 dark:text-gray-300 space-y-2">
                                 <li>Read books;</li>
+                                <li>Learn new languages;</li>
                                 <li>Travel around the world;</li>
                                 <li>Drink coffee;</li>
                                 <li>Gym;</li>
@@ -207,9 +218,9 @@ export const About = () => {
                             <h3 className="text-xl font-bold mb-4">Featured Certifications</h3>
                             <div className="space-y-4 text-gray-500 dark:text-gray-300">
                                 <div>
-                                    <h4>DevOps Career | AWS, Linux, Scripting, Jenkins, Ansible, GitOps, Docker, Kubernetes, & Terraform.</h4>
+                                    <h4>DevOps Beginners to Advanced</h4>
                                     <p className="text-gray-400 mb-2">Udemy</p>
-                                    <a href="/" className="text-blue-400 hover:text-blue-300 transition-colors my-4" target="_blank" rel="noopener noreferrer">In progress!</a>
+                                    <a href="https://www.udemy.com/certificate/UC-a9bb71e6-68c5-453d-ba64-4879ccdf0573/" className="text-blue-400 hover:text-blue-300 transition-colors my-4" target="_blank" rel="noopener noreferrer">View Credential →</a>
                                 </div>
                                 <div>
                                     <h4>ISTQB Certified Tester</h4>
